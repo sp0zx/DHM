78a2297422a45b5f6f3fc00527868a3f97ecbbe9
sp0zx <122081930+sp0zx@users.noreply.github.com>
2024-01-31 06:15:39 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,33 @@
-print("Hello World!")
+local CurrentCamera = game:GetService("Workspace").CurrentCamera
+local RunService = game:GetService("RunService")
+local Mouse = game.Players.LocalPlayer:GetMouse()
+local Plr
+function FindClosestPlayer()
+    local closestPlayer
+    local shortestDistance = math.huge
+for i, v in pairs(game.Players:GetPlayers()) doif v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChild("Humanoid") and
+v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") thenlocal pos = CurrentCamera:WorldToViewportPoint(v.Character.PrimaryPart.Position)
+            local magnitude = (Vector2.new(pos.X, pos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude
+            if magnitude < shortestDistance then
+                closestPlayer = v
+                shortestDistance = magnitude
+            end
+        end
+    end
+    return closestPlayer
+end
+Plr = FindClosestPlayer()
+print(Plr.Name)
+
+
+
+
+
+
+
+
+
+
+
+
+
